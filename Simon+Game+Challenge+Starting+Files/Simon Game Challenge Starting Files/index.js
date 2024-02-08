df50a4bf8c01a27feb65de1efab9887462f47cff
Avinash7770 <Avimaurya7770@gmail.com>
2024-02-08 14:25:53 +0530

    simon-update

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -1,18 +1,25 @@
- var x=1;
- $(document).keypress(function(){
-   if(x){
-      $("h1").text("LEVEL "+x);
-       x=0;
-    }
+var buttoncolors = ["red","blue","green","yellow"];
+var gamepattern=[];
+var userClickedPattern=[];
+var currentclick;
 
+function play(name){
+   var audio = new Audio("sounds/" + name + ".mp3");
+   audio.play();
+
+}
+function nextsequence(){
+  var current = Math.floor(Math.random()*4);
+  var randomchosencolor = buttoncolors[current];
+  gamepattern.push(randomchosencolor);
+  $("#"+randomchosencolor).fadeIn(100).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/" + randomchosencolor + ".mp3");
+  audio.play();
+ 
+}
+$(".btn").on("click",function(){
+   currentclick=$(this).attr("id");
+   userClickedPattern.push(currentclick);
+   play(currentclick);
 });
-
-
-
-
-
-
-
-
-
 
