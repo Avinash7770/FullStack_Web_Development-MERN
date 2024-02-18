82eb356364b875f44a8509999ca8683f39aef868
Avinash7770 <Avimaurya7770@gmail.com>
2024-02-18 18:47:35 +0530

    update

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -1,14 +1,29 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app=express();
 const port=3000;
-const d = new Date();
-let day = d.getDay();
-app.get("/", (req, res) => {
 
-    res.render(__dirname + "/public/index.ejs");
+
+app.get("/", (req, res) => {
+      const today1 = new Date();
+      const today11 = today1.getDay();
+      console.log(today11);
+      if(today11===0|| today11===6){
+        res.render("index.ejs",{
+      
+     
+          day:"a weekend",
+          advice:"Have Fun",
+         });
+      }
+      else{
+    res.render("index.ejs",{
+      
+     
+     day:"a weekday",
+     advice:"Work hard",
+    });
+  }
   });
 
 app.listen(port, () => {
