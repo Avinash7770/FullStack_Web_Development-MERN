5cea728819b122834c5cdb899f8fcc9ab3916d82
Avinash7770 <Avimaurya7770@gmail.com>
2024-03-06 20:29:29 +0530

    Put-patch

--- 01.BACKEND/6.0 DIY API/index.js
+++ 01.BACKEND/6.0 DIY API/index.js
@@ -14,18 +14,73 @@ app.get("/random",(req,res)=>{
 })
 
 //2. GET a specific joke
+app.get("/joke/:id",(req,res)=>{
+  const index=parseInt(req.params.id);
+  const foundjoke=jokes.find((joke)=>joke.id===index)
+  res.json(foundjoke);
+})
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter",(req,res)=>{
+  const joketype=req.body.type;
+  const typejoke=jokes.filter((joke)=>joke.type===joketype);
+  res.json(typejoke);
+
+})
+
 
 //4. POST a new joke
 
+app.post("/jokes",(req,res)=>{
+
+const newjoke={
+  id:jokes.length+1,
+  jokeText: req.body.text,
+  jokeType:req.body.type,
+};
+jokes.push(newjoke);
+console.log(jokes.slice(-1));
+res.json(newjoke);
+
+})
+
+
 //5. PUT a joke
+app.put("/jokes/:id",(req,res)=>{
+  const putid=parseInt(req.params.id);
+  const putjoke=jokes.find((joke)=>joke.id===putid);
+  putjoke.jokeText=req.body.text;
+  putjoke.jokeType=req.body.type;
+  res.json(putjoke);
+})
+
+
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
+
 
 //7. DELETE Specific joke
 
+
+
+
 //8. DELETE All jokes
+
+
+
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
